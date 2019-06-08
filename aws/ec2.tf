965770ead21965050855a3f3b5393ed513d33dbb
System Administrator <root@Nikhils-MacBook-Air.local>
2019-06-08 13:50:57 +0530

    ec2 updated

--- aws/ec2.tf
+++ aws/ec2.tf
@@ -1,16 +1,18 @@
+provider "aws" {
+  region = "eu-east-1"
+}
 resource "aws_instance" "MyTest" {
   ami           = "ami-098bb5d92c8886ca1"
   instance_type = "t2.micro"
-  key_name = "Your key name"
+  key_name = "Key name"
+  security_groups = [ "security group names" ]
   tags = {
     terraform = "true"
     env       = "Dev"
     Name      = "My First Server"
   }
-  root_block_device = [
-    {
+  root_block_device  {
       volume_type = "gp2"
       volume_size = 25
-    },
-  ]
+  }
 }
